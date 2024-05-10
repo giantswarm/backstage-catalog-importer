3206fbd7e4b758dc3761a54bffdd7aa4494333a8
Marian Steinbach <marian@giantswarm.io>
2024-05-10 16:47:37 +0200

    Create component using the Options pattern

--- pkg/catalog/functions.go
+++ pkg/catalog/functions.go
@@ -2,6 +2,7 @@ package catalog
 
 import (
 	"fmt"
+	"log"
 	"sort"
 	"strings"
 	"time"
@@ -10,6 +11,10 @@ import (
 	"github.com/giantswarm/backstage-catalog-importer/pkg/repositories"
 )
 
+// CreateComponentEntity is the deprecated way of generating a component entity.
+// It sets some vaules in a Giant Swarm specific way. We are replacing it, with
+// the goal to make this tool useful for more than just Giant Swarm.
+// Deprecated: Use the Component struct and its ToEntity() method instead.
 func CreateComponentEntity(r repositories.Repo,
 	team string,
 	description string,
@@ -22,151 +27,108 @@ func CreateComponentEntity(r repositories.Repo,
 	latestReleaseTag string,
 	charts []*helmchart.Chart,
 	dependsOn []string) Entity {
-	e := Entity{
-		APIVersion: "backstage.io/v1alpha1",
-		Kind:       EntityKindComponent,
-		Metadata: EntityMetadata{
-			Name:        r.Name,
-			Labels:      map[string]string{},
-			Description: description,
-			Annotations: map[string]string{
-				"github.com/project-slug":      fmt.Sprintf("giantswarm/%s", r.Name),
-				"github.com/team-slug":         team,
-				"backstage.io/source-location": fmt.Sprintf("url:https://github.com/giantswarm/%s", r.Name),
-				"quay.io/repository-slug":      fmt.Sprintf("giantswarm/%s", r.Name),
 
-				// Like team name, but without "team-" prefix
-				"opsgenie.com/team": strings.TrimPrefix(team, "team-"),
-			},
-			Tags: []string{},
-		},
+	// Possible deployment names for resource discovery via the Giant Swarm plugin,
+	// Grafana dashboards, and OpsGenie alerts.
+	deploymentNames := r.DeploymentNames
+
+	// Default deployment names are REPONAME and REPONAME-app.
+	if len(deploymentNames) == 0 {
+		name := strings.TrimSuffix(r.Name, "-app")
+		nameWithAppSuffix := fmt.Sprintf("%s-app", name)
+		deploymentNames = []string{
+			name,
+			nameWithAppSuffix,
+		}
+	}
+
+	// OpsGenie query
+	opsGenieQueryParts := []string{}
+	for _, d := range deploymentNames {
+		opsGenieQueryParts = append(opsGenieQueryParts, fmt.Sprintf("detailsPair(app:%s)", d))
+	}
+	opsGenieQuery := strings.Join(opsGenieQueryParts, " OR ")
+
+	c, err := NewComponent(r.Name,
+		WithDescription(description),
+		WithGithubProjectSlug(fmt.Sprintf("giantswarm/%s", r.Name)),
+		WithGithubTeamSlug(team),
+		WithOpsGenieTeam(strings.TrimPrefix(team, "team-")),
+		WithQuayRepositorySlug(fmt.Sprintf("giantswarm/%s", r.Name)),
+		WithLatestReleaseTag(latestReleaseTag),
+		WithLatestReleaseTime(latestReleaseTime),
+		WithCircleCiSlug(fmt.Sprintf("github/giantswarm/%s", r.Name)),
+		WithDefaultBranch(defaultBranch),
+		WithHasReadme(hasReadme),
+		WithDeploymentNames(deploymentNames...),
+		WithOpsGenieComponentSelector(opsGenieQuery),
+		WithSystem(system),
+		WithType(r.ComponentType),
+		WithOwner(team),
+		WithDependsOn(dependsOn...),
+	)
+	if err != nil {
+		log.Fatalf("Could not create component: %s", err)
 	}
 
 	// Additional metadata
-	{
-		if latestReleaseTag != "" {
-			e.Metadata.Annotations["giantswarm.io/latest-release-tag"] = latestReleaseTag
-			e.Metadata.Annotations["giantswarm.io/latest-release-date"] = latestReleaseTime.Format(time.RFC3339)
+
+	if r.Gen.Language != "" && r.Gen.Language != repositories.RepoLanguageGeneric {
+		c.SetLabel("giantswarm.io/language", string(r.Gen.Language))
+		c.AddTag(fmt.Sprintf("language:%s", r.Gen.Language))
+	}
+
+	if isPrivate {
+		c.AddTag("private")
+	}
+
+	if defaultBranch == "master" {
+		c.AddTag("defaultbranch:master")
+	}
+
+	if latestReleaseTag == "" {
+		c.AddTag("no-releases")
+	}
+
+	if len(charts) > 0 {
+		c.AddTag("helmchart")
+
+		names := []string{}
+		versions := []string{}
+		appVersions := []string{}
+		for _, c := range charts {
+			names = append(names, c.Metadata.Name)
+			versions = append(versions, c.Metadata.Version)
+			appVersions = append(appVersions, c.Metadata.AppVersion)
 		}
 
-		if hasCircleCi {
-			e.Metadata.Annotations["circleci.com/project-slug"] = fmt.Sprintf("github/giantswarm/%s", r.Name)
-		}
+		c.SetAnnotation("giantswarm.io/helmcharts", strings.Join(names, ","))
+		c.SetAnnotation("giantswarm.io/helmchart-versions", strings.Join(versions, ","))
+		c.SetAnnotation("giantswarm.io/helmchart-app-versions", strings.Join(appVersions, ","))
+	}
 
-		if hasReadme && defaultBranch != "" {
-			e.Metadata.Annotations["backstage.io/techdocs-ref"] = fmt.Sprintf("url:https://github.com/giantswarm/%s/tree/%s", r.Name, defaultBranch)
-		}
+	for _, flavor := range r.Gen.Flavors {
+		c.SetLabel(fmt.Sprintf("giantswarm.io/flavor-%s", flavor), "true")
+		c.AddTag(fmt.Sprintf("flavor:%s", flavor))
+	}
 
-		// Possible deployment names for resource discovery via the Giant Swarm plugin,
-		// Grafana dashboards, and OpsGenie alerts.
-		deploymentNames := r.DeploymentNames
-		sort.Strings(deploymentNames)
-
-		// Default deployment names are REPONAME and REPONAME-app.
-		if len(deploymentNames) == 0 {
-			name := strings.TrimSuffix(r.Name, "-app")
-			nameWithAppSuffix := fmt.Sprintf("%s-app", name)
-			deploymentNames = []string{
-				name,
-				nameWithAppSuffix,
-			}
-		}
-
-		e.Metadata.Annotations["giantswarm.io/deployment-names"] = strings.Join(deploymentNames, ",")
-
-		// OpsGenie query
-		queryParts := []string{}
+	// Grafana dashboard link
+	if r.ComponentType == "service" {
+		urlParts := []string{}
 		for _, d := range deploymentNames {
-			queryParts = append(queryParts, fmt.Sprintf("detailsPair(app:%s)", d))
+			urlParts = append(urlParts, fmt.Sprintf("var-app=%s", d))
 		}
-		e.Metadata.Annotations["opsgenie.com/component-selector"] = strings.Join(queryParts, " OR ")
-
-		if r.Gen.Language != "" && r.Gen.Language != repositories.RepoLanguageGeneric {
-			e.Metadata.Labels["giantswarm.io/language"] = string(r.Gen.Language)
-			e.Metadata.Tags = append(e.Metadata.Tags, fmt.Sprintf("language:%s", r.Gen.Language))
-		}
-
-		if isPrivate {
-			e.Metadata.Tags = append(e.Metadata.Tags, "private")
-		}
-
-		if defaultBranch == "master" {
-			e.Metadata.Tags = append(e.Metadata.Tags, "defaultbranch:master")
-		}
-
-		if latestReleaseTag == "" {
-			e.Metadata.Tags = append(e.Metadata.Tags, "no-releases")
-		}
-
-		if len(charts) > 0 {
-			e.Metadata.Tags = append(e.Metadata.Tags, "helmchart")
-			names := []string{}
-			versions := []string{}
-			appVersions := []string{}
-			for _, c := range charts {
-				names = append(names, c.Metadata.Name)
-				versions = append(versions, c.Metadata.Version)
-				appVersions = append(appVersions, c.Metadata.AppVersion)
-			}
-
-			e.Metadata.Annotations["giantswarm.io/helmcharts"] = strings.Join(names, ",")
-			e.Metadata.Annotations["giantswarm.io/helmchart-versions"] = strings.Join(versions, ",")
-			e.Metadata.Annotations["giantswarm.io/helmchart-app-versions"] = strings.Join(appVersions, ",")
-		}
-
-		for _, flavor := range r.Gen.Flavors {
-			e.Metadata.Labels[fmt.Sprintf("giantswarm.io/flavor-%s", flavor)] = "true"
-			e.Metadata.Tags = append(e.Metadata.Tags, fmt.Sprintf("flavor:%s", flavor))
-		}
-
-		sort.Strings(e.Metadata.Tags)
-
-		if r.ComponentType == "service" {
-			// Kubernetes plugin annotation
-			e.Metadata.Annotations["backstage.io/kubernetes-id"] = r.Name
-
-			// Grafana dashboard links
-			urlParts := []string{}
-			for _, d := range deploymentNames {
-				urlParts = append(urlParts, fmt.Sprintf("var-app=%s", d))
-			}
-			e.Metadata.Links = []EntityLink{
-				{
-					URL:   fmt.Sprintf("https://giantswarm.grafana.net/d/eb617ba1-209a-4d57-9963-1af9a8ddc8d4/general-service-metrics?orgId=1&%s&from=now-24h&to=now", strings.Join(urlParts, "&")),
-					Title: "General service metrics dashboard",
-					Icon:  "dashboard",
-					Type:  "grafana-dashboard",
-				},
-			}
-		}
+		c.AddLink(EntityLink{
+			URL:   fmt.Sprintf("https://giantswarm.grafana.net/d/eb617ba1-209a-4d57-9963-1af9a8ddc8d4/general-service-metrics?orgId=1&%s&from=now-24h&to=now", strings.Join(urlParts, "&")),
+			Title: "General service metrics dashboard",
+			Icon:  "dashboard",
+			Type:  "grafana-dashboard",
+		})
 	}
 
-	// Entity spec
+	e := c.ToEntity()
 
-	spec := ComponentSpec{
-		Type:      "unspecified",
-		Lifecycle: "production",
-		Owner:     team,
-		System:    system,
-	}
-
-	if r.ComponentType != "" {
-		spec.Type = r.ComponentType
-	}
-
-	if r.Lifecycle != "production" && r.Lifecycle != "" {
-		spec.Lifecycle = string(r.Lifecycle)
-	}
-
-	if len(dependsOn) > 0 {
-		for _, d := range dependsOn {
-			spec.DependsOn = append(spec.DependsOn, "component:"+d)
-		}
-	}
-
-	e.Spec = spec
-
-	return e
+	return *e
 }
 
 func CreateGroupEntity(name, displayName, description, parent string, members []string, id int64) Entity {
