cac4c1dd09a241a20c61f55d2b1ab61a3dd7e318
Marian Steinbach <marian@giantswarm.io>
2024-05-15 09:53:16 +0200

    Add appcatalog package

---
+++ pkg/appcatalog/appcatalog_test.go
@@ -0,0 +1,42 @@
+// Package appcatalog provides a means to read information form a
+// Giant Swarm app catalog, which is technically a Helm repository
+// with conventional metadata.
+package appcatalog
+
+import (
+	"os"
+	"testing"
+)
+
+func TestRead(t *testing.T) {
+	tests := []struct {
+		name       string
+		path       string
+		numEntries int
+		wantErr    bool
+	}{
+		{
+			name:       "giantswarm catalog",
+			path:       "testdata/giantswarm.yaml",
+			numEntries: 2,
+			wantErr:    false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			bytes, err := os.ReadFile(tt.path)
+			if err != nil {
+				t.Errorf("Read() error = %v", err)
+				return
+			}
+			f, err := Load(bytes)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Read() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if len(f.Entries) != tt.numEntries {
+				t.Errorf("Read() got %d repositories, want %d", len(f.Entries), tt.numEntries)
+			}
+		})
+	}
+}
