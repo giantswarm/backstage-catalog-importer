43e6d39f91141bbea85aadc6c81e714dd3651457
Marian Steinbach <marian@giantswarm.io>
2024-07-09 16:05:40 +0200

    Ensure stable sorting

--- pkg/output/export/export.go
+++ pkg/output/export/export.go
@@ -3,9 +3,11 @@ package export
 
 import (
 	"bytes"
+	"cmp"
 	"os"
+	"slices"
 
-	"gopkg.in/yaml.v3"
+	yaml "gopkg.in/yaml.v3"
 
 	bscatalog "github.com/giantswarm/backstage-catalog-importer/pkg/output/bscatalog/v1alpha1"
 )
@@ -14,6 +16,8 @@ const separator = "---\n"
 
 type Service struct {
 	TargetPath string
+
+	collection []*bscatalog.Entity
 	buffer     bytes.Buffer
 }
 
@@ -22,36 +26,59 @@ type Config struct {
 	TargetPath string
 }
 
+// New returns a new export service.
+// It is used to collect entities, sort them, and write them to a file.
+// For testing convenience, the collection content is stored in a buffer.
+// The entity sort order is: APIVersion, Kind, Metadata.Namespace, Metadata.Name.
 func New(config Config) *Service {
 	s := &Service{
 		TargetPath: config.TargetPath,
 	}
 
+	return s
+}
+
+func (s *Service) updateBuffer() error {
+	s.buffer.Reset()
 	_, _ = s.buffer.WriteString("#\n# This file was generated automatically. PLEASE DO NOT MODIFY IT BY HAND!\n#\n\n")
 
-	return s
+	for i := range s.collection {
+		yamlBytes, err := yaml.Marshal(&s.collection[i])
+		if err != nil {
+			return err
+		}
+		_, err = s.buffer.WriteString(separator)
+		if err != nil {
+			return err
+		}
+		_, err = s.buffer.Write(yamlBytes)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 // Adds an entity to the export buffer
 func (s *Service) AddEntity(entity *bscatalog.Entity) error {
-	yamlBytes, err := yaml.Marshal(&entity)
-	if err != nil {
-		return err
-	}
-	_, err = s.buffer.WriteString(separator)
-	if err != nil {
-		return err
-	}
-	_, err = s.buffer.Write(yamlBytes)
-	if err != nil {
-		return err
-	}
+	s.collection = append(s.collection, entity)
+
+	slices.SortFunc(s.collection, func(a, b *bscatalog.Entity) int {
+		return cmp.Or(
+			cmp.Compare(a.APIVersion, b.APIVersion),
+			cmp.Compare(a.Kind, b.Kind),
+			cmp.Compare(a.Metadata.Namespace, b.Metadata.Namespace),
+			cmp.Compare(a.Metadata.Name, b.Metadata.Name),
+		)
+	})
 
 	return nil
 }
 
 // Returns the current length of the buffer.
 func (s *Service) Len() int {
+	_ = s.updateBuffer()
 	return s.buffer.Len()
 }
 
@@ -63,6 +90,11 @@ func (s *Service) WriteFile() error {
 	}
 	defer file.Close()
 
+	err = s.updateBuffer()
+	if err != nil {
+		return err
+	}
+
 	_, err = file.WriteString(s.buffer.String())
 	if err != nil {
 		return err
@@ -71,5 +103,6 @@ func (s *Service) WriteFile() error {
 }
 
 func (s *Service) String() string {
+	_ = s.updateBuffer()
 	return s.buffer.String()
 }
